7d7067f7dc9beaffbb31a32c9d14997050fbd39a
Brendon <torre.brendon76@gmail.com>
2018-05-03 18:35:09 +0200

    Resize hamburger svg for menuLowRes

--- src/MenuLowRes.js
+++ src/MenuLowRes.js
@@ -27,7 +27,7 @@ class MenuLowRes extends Component {
     return (
       <div className="menuLowRes">
         <div key={this.state.left} className="round" onClick={this.calcProgress.bind(this)} style={{transform: `translate3d(${this.state.x}px, 0, 0)`}}>
-          <svg viewBox="0 0 850 650" width="50" height="50" className={`mlr ${this.state.menu}`} >
+          <svg viewBox="0 0 850 650" width="60" height="60" className={`mlr ${this.state.menu}`} >
             <path d="M300,220 C300,220 520,220 540,220 C740,220 640,540 520,420 C440,340 300,200 300,200" className="top_bar" />
             <path d="M300,320 L540,320" className="middle_bar" />
             <path d="M300,210 C300,210 520,210 540,210 C740,210 640,530 520,410 C440,330 300,190 300,190" className="bottom_bar" transform="translate(480, 320) scale(1, -1) translate(-480, -318)" />
