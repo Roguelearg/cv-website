87b7641f9e188e32244e0ab65d5394357dfae733
roguelearg <torre.brendon76@gmail.com>
2018-05-04 16:12:20 +0200

    Add some shadows for langs div

--- src/Competences.js
+++ src/Competences.js
@@ -7,32 +7,36 @@ const Competences = () => {
   return (
     <div>
       <h1>Compétences</h1>
-      <h3>Languages</h3>
-      <div className="competences">
-        <div className="langs">
-          <Competence langage="HTML/CSS" niveau="95"/>
-          <Competence langage="Ruby" niveau="90"/>
-          <Competence langage="Javascript" niveau="90"/>
-          <Competence langage="Java" niveau="80"/>
-          <Competence langage="Bash" niveau="80"/>
-          <Competence langage="PostgreSQL" niveau="70"/>
-          <Competence langage="C/C++" niveau="60"/>
+      <div className="part">
+        <h3>Languages</h3>
+        <div className="competences">
+          <div className="langs">
+            <Competence langage="HTML/CSS" niveau="95"/>
+            <Competence langage="Ruby" niveau="90"/>
+            <Competence langage="Javascript" niveau="90"/>
+            <Competence langage="Java" niveau="80"/>
+            <Competence langage="Bash" niveau="80"/>
+            <Competence langage="PostgreSQL" niveau="70"/>
+            <Competence langage="C/C++" niveau="60"/>
+          </div>
         </div>
+        <p className="come"> À venir : PHP, C#</p>
       </div>
-      <p className="come"> À venir : PHP, C#</p>
-      <h3>Technologies</h3>
-      <div className="competences">
-        <div className="langs">
-          <Competence langage="Git/Bitbucket" niveau="90"/>
-          <Competence langage="Ruby on Rails" niveau="90"/>
-          <Competence langage="Linux" niveau="85"/>
-          <Competence langage="React" niveau="85"/>
-          <Competence langage="Docker" niveau="80"/>
-          <Competence langage="Heroku" niveau="80"/>
-          <Competence langage="Android" niveau="65"/>
+      <div className="part">
+        <h3>Technologies</h3>
+        <div className="competences">
+          <div className="langs">
+            <Competence langage="Git/Bitbucket" niveau="90"/>
+            <Competence langage="Ruby on Rails" niveau="90"/>
+            <Competence langage="Linux" niveau="85"/>
+            <Competence langage="React" niveau="85"/>
+            <Competence langage="Docker" niveau="80"/>
+            <Competence langage="Heroku" niveau="80"/>
+            <Competence langage="Android" niveau="65"/>
+          </div>
         </div>
+        <p className="come">À venir : Unity</p>
       </div>
-      <p className="come">À venir : Unity</p>
     </div>
   );
 }
